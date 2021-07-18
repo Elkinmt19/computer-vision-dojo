a30aa0319bd483f380b3054fe31b8e9b58aee673
Elkinmt19 <elkin02011@hotmail.com>
2021-07-18 10:01:13 -0500

    Made the python package information in the setup.py file

---
+++ python/class-challenges/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+# Get README.md long description to add it to the builded package
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+
+    # Library name (same as "pip install ____")
+    name='class_challenges',
+
+    author='Elkin Guerra',
+
+    # Package version (MAJOR_VERSION.MINOR_VERSION.MAINTENANCE_VERSION)
+    version='0.0.1',
+
+    url="https://github.com/Elkinmt19/computer-vision-dojo",
+
+    # Simple package description
+    description= "Computer vision library containing challenges from EIA University's computer vision class.",
+
+    # Long package description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    # Our main folder to build the package
+    package_dir={'': 'src'},
+
+    # Add library dependencies
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "opencv-python==4.5.3.56",
+    ],
+
+    # Add devlopment dependencies
+    extras_require={
+        "dev": [
+            "pytest",
+        ],
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha"
+    ],
+
+    license="MIT License",
+
+    # Add extra XML and JSONs needed
+    include_package_data=True
+)
