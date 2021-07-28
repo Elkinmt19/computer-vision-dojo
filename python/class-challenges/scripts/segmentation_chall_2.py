c5376e113112e1f02806dd64f4fa1488fd4b286e
Elkinmt19 <elkin02011@hotmail.com>
2021-07-28 13:12:19 -0500

    Finish the second topic of the second challenge:Topic: image segmentation

--- python/class-challenges/scripts/segmentation_chall_2.py
+++ python/class-challenges/scripts/segmentation_chall_2.py
@@ -1,11 +1,13 @@
 # Built-int imports 
 import os
 import sys
+import math
 import argparse
 
 # External imports
 import cv2 as cv
 import numpy as np
+from numpy.core.fromnumeric import size
 
 # My own imports 
 import get_path_assests_folder as gpaf
@@ -19,14 +21,15 @@ class CoinSegmentation:
     
     """
     def __init__(self):
-        self.__coins_path = self.get_image_path()
+        self.__coins_path = self.get_image_path("monedas.jpg")
         self.__image = cv.imread(self.__coins_path, 1)
+        self.__gray_image = cv.imread(self.__coins_path, 0)
         self.__rows, self.__cols, self.__channels = self.__image.shape
 
 
-    def get_image_path(self):
+    def get_image_path(self, image_name):
         image_relative_path = os.path.join(
-        ASSETS_FOLDER, "imgs", "monedas.jpg")
+        ASSETS_FOLDER, "imgs", image_name)
         return image_relative_path
 
     def nothing(self, x):
@@ -35,23 +38,23 @@ class CoinSegmentation:
         """
         pass
 
-    def create_trackbar(self,name):
+    def create_trackbar(self,name,title):
         return cv.createTrackbar(
             name,
-            "Coins grup segmentation",
+            title,
             0,
             255,
             self.nothing
         )
 
-    def segmentation(self):
+    def color_segmentation(self):
         cv.namedWindow("Coins grup segmentation")
-        self.create_trackbar("Blue-u")
-        self.create_trackbar("Blue-l")
-        self.create_trackbar("Green-u")
-        self.create_trackbar("Green-l")
-        self.create_trackbar("Red-u")
-        self.create_trackbar("Red-l")
+        self.create_trackbar("Blue-u", "Coins grup segmentation")
+        self.create_trackbar("Blue-l","Coins grup segmentation")
+        self.create_trackbar("Green-u","Coins grup segmentation")
+        self.create_trackbar("Green-l","Coins grup segmentation")
+        self.create_trackbar("Red-u","Coins grup segmentation")
+        self.create_trackbar("Red-l","Coins grup segmentation")
 
         image = self.__image.copy()
 
@@ -100,6 +103,59 @@ class CoinSegmentation:
         cv.imshow("Coin's groups", image)
         cv.waitKey(0)
         cv.destroyAllWindows()
+
+    def binarization(self):
+        cv.namedWindow("Coins binarization")
+        self.create_trackbar("Gray-u","Coins binarization")
+        self.create_trackbar("Gray-l","Coins binarization")
+
+        image = self.__gray_image.copy()
+
+        while(True):
+            var_gray_upper = cv.getTrackbarPos("Gray-u","Coins binarization")
+            var_gray_lower = cv.getTrackbarPos("Gray-l","Coins binarization")
+    
+            for i in iter(range(self.__rows)):
+                for j in iter(range(self.__cols)):
+                    if (var_gray_upper > image[i,j] and var_gray_lower < image[i,j]):
+                        image[i,j] = 255
+                    else:
+                        image[i,j] = 0
+                        
+            cv.imshow("Coins binarization", image)
+            image = self.__gray_image.copy()
+
+            if (cv.waitKey(1) & 0xFF == ord('q')):
+                break
+        cv.destroyAllWindows()
+    
+    def coin_count(self):
+        image = self.__gray_image.copy()
+        total_area = 0
+
+        for i in iter(range(self.__rows)):
+            for j in iter(range(self.__cols)):
+                if (image[i,j] > 19 and image[i,j] < 171):
+                    total_area += 1
+        
+        print(f"The total area is: {total_area} pixel^2")
+        coin_size = total_area/8
+        print(f"The average size of the coins is: {coin_size} pixel^2")
+
+        other_image = cv.imread(
+            self.get_image_path("monedas2.jpg"),
+            0
+        )
+
+        other_img_area = 0
+        for i in iter(range(self.__rows)):
+            for j in iter(range(self.__cols)):
+                if (other_image[i,j] > 19 and other_image[i,j] < 171):
+                    other_img_area += 1
+
+        print(f"Number of coins: {int(other_img_area/coin_size)}")
+
+
 
 
 def main():
