91a987e85f91eecb2165eb8367361e29da4d7fde
Elkinmt19 <elkin02011@hotmail.com>
2021-07-28 10:41:42 -0500

    Finish the first topic of the second challenge: Topic: image segmentation

--- python/class-challenges/scripts/segmentation_chall_2.py
+++ python/class-challenges/scripts/segmentation_chall_2.py
@@ -35,32 +35,39 @@ class CoinSegmentation:
         """
         pass
 
-    def group_coin_segmentation(self):
+    def create_trackbar(self,name):
+        return cv.createTrackbar(
+            name,
+            "Coins grup segmentation",
+            0,
+            255,
+            self.nothing
+        )
+
+    def segmentation(self):
         cv.namedWindow("Coins grup segmentation")
-        cv.createTrackbar(
-            "Blue-upper",
-            "Coins grup segmentation",
-            0,
-            255,
-            self.nothing
-        )
-        cv.createTrackbar(
-            "Blue-lower",
-            "Coins grup segmentation",
-            0,
-            255,
-            self.nothing
-        )
+        self.create_trackbar("Blue-u")
+        self.create_trackbar("Blue-l")
+        self.create_trackbar("Green-u")
+        self.create_trackbar("Green-l")
+        self.create_trackbar("Red-u")
+        self.create_trackbar("Red-l")
 
         image = self.__image.copy()
 
         while(True):
-            var_blue_upper = cv.getTrackbarPos("Blue-upper", "Coins grup segmentation")
-            var_blue_lower = cv.getTrackbarPos("Blue-lower", "Coins grup segmentation")
+            var_blue_upper = cv.getTrackbarPos("Blue-u", "Coins grup segmentation")
+            var_blue_lower = cv.getTrackbarPos("Blue-l", "Coins grup segmentation")
+            var_green_upper = cv.getTrackbarPos("Green-u", "Coins grup segmentation")
+            var_green_lower = cv.getTrackbarPos("Green-l", "Coins grup segmentation")
+            var_red_upper = cv.getTrackbarPos("Red-u", "Coins grup segmentation")
+            var_red_lower = cv.getTrackbarPos("Red-l", "Coins grup segmentation")
 
             for i in iter(range(self.__rows)):
                 for j in iter(range(self.__cols)):
-                    if (var_blue_upper > image[i,j,0] and var_blue_lower < image[i,j,2]):
+                    if (var_blue_upper > image[i,j,0] and var_blue_lower < image[i,j,0] and \
+                        var_green_upper > image[i,j,1] and var_green_lower < image[i,j,1] and \
+                        var_red_upper > image[i,j,2] and var_red_lower < image[i,j,2]):
                         image[i,j] = 255
                     else:
                         image[i,j] = 0
@@ -70,6 +77,29 @@ class CoinSegmentation:
 
             if (cv.waitKey(1) & 0xFF == ord('q')):
                 break
+        cv.destroyAllWindows()
+    
+    def group_coin_segmentation(self):
+        image = self.__image.copy()
+
+        for i in iter(range(self.__rows)):
+            for j in iter(range(self.__cols)):
+                if (image[i,j,0] > 50 and image[i,j,0] < 111 and \
+                    image[i,j,1] > 101 and image[i,j,1] < 149 and \
+                    image[i,j,2] > 137 and image[i,j,2] < 215):
+                    image[i,j] = np.array([255,0,0])
+                elif (image[i,j,0] > 23 and image[i,j,0] < 72 and \
+                    image[i,j,1] > 64 and image[i,j,1] < 100 and \
+                    image[i,j,2] > 75 and image[i,j,2] < 111):
+                    image[i,j] = np.array([0,255,0])
+                elif (image[i,j,0] > 28 and image[i,j,0] < 60 and \
+                    image[i,j,1] > 71 and image[i,j,1] < 106 and \
+                    image[i,j,2] > 119 and image[i,j,2] < 131):
+                    image[i,j] = np.array([0,0,255])
+
+        cv.imshow("Coin's groups", image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
 
 
 def main():
