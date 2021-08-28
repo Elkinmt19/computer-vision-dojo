a6bea35fbfa49473400aaaec4725f8949f3d0f83
Elkinmt19 <elkin02011@hotmail.com>
2021-08-27 20:48:16 -0500

    Added the 'robotnik_coppila.lua' file which is the scripts that has to be running at coppelia in order to be able to establish a communication between coppelia and python

---
+++ python/coppelia-robotics/scripts/robotnik_coppelia.lua
@@ -0,0 +1,21 @@
+function sysCall_init()
+    corout=coroutine.create(coroutineMain)
+end
+
+function sysCall_actuation()
+    if coroutine.status(corout)~='dead' then
+        local ok,errorMsg=coroutine.resume(corout)
+        if errorMsg then
+            error(debug.traceback(corout,errorMsg),2)
+        end
+    end
+end
+
+function coroutineMain()
+end
+
+function sysCall_cleanup()
+
+end
+
+simRemoteApi.start(19999)
