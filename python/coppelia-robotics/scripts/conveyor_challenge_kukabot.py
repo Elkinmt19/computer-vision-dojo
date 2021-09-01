7bfbad3d1cf630b2bd996e0cdc64c460c1c7e5e2
Elkinmt19 <elkin02011@hotmail.com>
2021-09-01 17:42:00 -0500

    Finish the first taks of the conveyor challenge, in this stage the script is able to detect and count the number of object, segment these objects per their color and check their quality

--- python/coppelia-robotics/scripts/conveyor_challenge_kukabot.py
+++ python/coppelia-robotics/scripts/conveyor_challenge_kukabot.py
@@ -4,6 +4,7 @@ import sys
 # External imports 
 import cv2 as cv
 import numpy as np
+from numpy.lib.index_tricks import index_exp
 
 # Own imports 
 import sim
@@ -59,11 +60,78 @@ class ConveyorChallenge:
             sim.simx_opmode_buffer
         )
 
+    def get_image_cube_camera(self):
+        number_cubes = [0,0,0,0]
+        faulty_cubes = [0,0,0,0]
+
+        # Preprocessing of the image
+        img = np.array(self.image, dtype = np.uint8)
+        img.resize([self.resolution[0], self.resolution[1], 3])
+        img = np.rot90(img,2)
+        img = np.fliplr(img)
+        img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+
+        # Umbral's values for the segmentation of the objects
+        LOWER_VALUES = np.array(
+            [
+                [0, 142, 114],
+                [125, 0, 0],
+                [0, 0, 132],
+                [0, 130, 0]
+            ]
+        )
+        UPPER_VALUES = np.array(
+            [
+                [50, 252, 255],
+                [255, 164, 129],
+                [97, 77, 255],
+                [62, 255, 131]
+            ]
+        )
+
+        for i in iter(range(4)):
+            binary_image = cv.inRange(img, LOWER_VALUES[i], UPPER_VALUES[i])
+
+            # Find the contours of the image
+            contours, hier = cv.findContours(
+                binary_image.copy(),
+                cv.RETR_TREE,
+                cv.CHAIN_APPROX_NONE
+            )
+
+            # Draw the comtours in the image
+            contour_image = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+
+            for cnt in iter(range(len(contours))):
+                if (hier[0][cnt][3] < 0):
+                    cv.drawContours(contour_image, contours[cnt], -1, (0,0,255), 2)
+                    number_cubes[i] += 1
+                    if (hier[0][cnt][2] > 0):
+                        faulty_cubes[i] += 1
+
+            cv.imshow("Cubes detected", contour_image)
+            cv.waitKey(500)
+
+        print(f"The Quantity of objects is:")
+        print(f"Yellow: {number_cubes[0]}")
+        print(f"Blue: {number_cubes[1]}")
+        print(f"Red: {number_cubes[2]}")
+        print(f"Green: {number_cubes[3]}")
+
+        print(f"The Quantity of faulty objects is:")
+        print(f"Yellow: {faulty_cubes[0]}")
+        print(f"Blue: {faulty_cubes[1]}")
+        print(f"Red: {faulty_cubes[2]}")
+        print(f"Green: {faulty_cubes[3]}")
+
+
     def cube_initial_diagnostic(self):
-        pass
+        self.cube_camera_buffer()
+        self.get_image_cube_camera()
 
 def main():
-    pass
+    conveyor_chall = ConveyorChallenge()
+    conveyor_chall.cube_initial_diagnostic()
 
 if __name__ == "__main__":
     sys.exit(main())
