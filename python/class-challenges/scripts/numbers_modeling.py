25d49e9c3256135b4587905d10de9e5711f9bcfa
Elkinmt19 <elkin02011@hotmail.com>
2021-10-01 11:46:13 -0500

    Deleted a useless import in the 'numbers_moding.py' file

--- python/class-challenges/scripts/numbers_modeling.py
+++ python/class-challenges/scripts/numbers_modeling.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler
 import joblib
 
 # My Own imports
