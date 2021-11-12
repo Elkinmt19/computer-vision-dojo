c2fce3b0ca09108819d3b54d5c67b33562284a5b
Elkinmt19 <elkin02011@hotmail.com>
2021-11-12 00:08:56 -0500

    Added some other features to the autopilot main script in order to make a trajectory using the mobile robot

--- python/computer-vision-final-project/scripts/auto_pilot.py
+++ python/computer-vision-final-project/scripts/auto_pilot.py
@@ -2,6 +2,9 @@
 import sys 
 import time
 
+# External imports
+import numpy as np
+
 # Own imports 
 import sim
 import robot_trajectory_controller as rc
@@ -15,12 +18,25 @@ class AutoPilot:
         # Define control variables
         self.controller = rc.TrajectoryController(self.__clientID)
         self.control_period = 0.1
-        self.error = [0.0,0.0,0.0,0.0]
-        self.it_acum = [0.0,0.0]
-        self.setpoint = [11.775,-4.8239] 
+        self.error = [0.0,0.0,0.0,0.0,0.0,0.0]
+        self.it_acum = [0.0,0.0,0.0]
+        self.setpoint = [11.250,-9.89,-1.5] 
         self.eps = 0.001
-        self.v = [0,0]
-        self.w = 0
+
+        # Trajectory pointer
+        self.pointer = 0
+        self.coor_count = 0
+
+        # Define trajectory coordinates
+        self.trajectory_coordinates = [
+            (11.5,1.5),
+            (8,1),
+            (1,0.8),
+            (1,-11),
+            (5,-5)
+        ]
+
+        self.trajectory_orientation = [-np.pi/2,-np.pi/2,0.1,np.pi/2,0.1]
 
     def start_connection(self):
         # End connection 
@@ -56,40 +72,81 @@ class AutoPilot:
         while (1):
             t0 = time.time()
             # Execute the avoid obstacle method
-            self.avoid_obstacles_controller()
+            # self.avoid_obstacles_controller()
+
+            # Update setpoint values
+            self.setpoint[0:2] = [
+                self.trajectory_coordinates[self.coor_count][0],
+                self.trajectory_coordinates[self.coor_count][1]
+            ]
+
+            self.setpoint[2] = self.trajectory_orientation[self.coor_count]
 
             # Only proceed to control calculation in correct sample time multiple
             sample_time_condition = time.time() - last_time >= self.control_period
             if (sample_time_condition):
+                # Go to goal controller (X, Y)
                 v, buff_error, it_term_k_1 = self.controller.go_to_goal(
-                    self.setpoint,
+                    self.setpoint[0:2],
                     [self.error[0],self.error[2]],
-                    self.it_acum,
+                    self.it_acum[0:2],
                     self.control_period
                 )
-                self.error[0], self.error[2] = (buff_error[0], buff_error[1])
 
-                wheel_speed = self.controller.mobile_robot_model(v[0],v[1],0)
+                # Go to angle controller (Theta)
+                w, buff_error_angle, it_term_k_1_angle = self.controller.go_to_angle(
+                    self.setpoint[2],
+                    self.error[4],
+                    self.it_acum[2],
+                    self.control_period
+                ) 
+
+                self.error[0], self.error[2], self.error[4] = (buff_error[0], buff_error[1], buff_error_angle)
+
+                if (self.pointer == 0):
+                    wheel_speed = self.controller.mobile_robot_model(v[0],v[1],0)
+                elif (self.pointer == 1):
+                    wheel_speed = self.controller.mobile_robot_model(v[1],-v[0],0)
+                elif (self.pointer == 2):
+                    wheel_speed = self.controller.mobile_robot_model(-v[0],-v[1],0)
+                elif (self.pointer == 3):
+                    wheel_speed = self.controller.mobile_robot_model(-v[1],v[0],0)
 
                 # Control condition action for x position control
                 x_condition = (abs(self.error[0]) >= 0 - self.eps and abs(self.error[0]) <= 0 + self.eps)
                 # Control condition action for x position control
                 y_condition = (abs(self.error[2]) >= 0 - self.eps and abs(self.error[2]) <= 0 + self.eps)
+                # Control condition action for angle position
+                angle_condition = (abs(self.error[4]) >= 0 - self.eps and abs(self.error[4]) <= 0 + self.eps)
 
                 if (x_condition and y_condition):
-                    self.controller.robot.move_mobile_robot_motors([0,0,0,0])
+                    wheel_speed = self.controller.mobile_robot_model(0,0,w)
+                    self.controller.robot.move_mobile_robot_motors(wheel_speed)
+                    if (angle_condition):
+                        self.pointer += 1
+                        self.coor_count += 1
+                        self.controller.robot.move_mobile_robot_motors([0,0,0,0])
                 else:
                     self.controller.robot.move_mobile_robot_motors(wheel_speed)
 
-                # Validation of the avoid obstacles algorithm
-                self.controller.avoid_obstacles(self.avoid_obs_controller.cameras)
+                # Special validation
+                if (self.pointer >= 4):
+                    self.pointer = 0
 
-                print(self.avoid_obs_controller.cameras)
+                # End condition
+                if (self.coor_count >= len(self.trajectory_coordinates)):
+                    break
+
+                # Validation of the avoid obstacles algorithm
+                # self.controller.avoid_obstacles(self.avoid_obs_controller.cameras)
+
+                # print(self.avoid_obs_controller.cameras)
 
                 self.it_acum[0] += it_term_k_1[0]           
                 self.it_acum[1] += it_term_k_1[1]
+                self.it_acum[2] += it_term_k_1_angle
                 last_time = time.time()
-            print(time.time()-t0)
+            # print(time.time()-t0)
 
         self.start_connection()
 
